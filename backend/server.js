23d04d8cce7b027f5cc2fffb9449c11608a0144d
Omar Meabed <omarmeabed4@gmail.com>
2022-08-14 02:21:29 -0700

    bobo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 const express = require("express");
 
 const app = express();
 
-app.listen(4000, ()=>{
-    console.log("hellooooo im  listennning");
+app.use((req, res, next)=>{
+    console.log(req.path, req.method);
+    next();
+});
+
+app.get("/", (req, res,)=>{
+    res.json({
+        message:"yooo hooo droooogoz"
+    });    
+});
+
+app.listen(process.env.PORT, ()=>{
+    console.log("hellooooo i am  listennning to ",process.env.PORT);
 });
