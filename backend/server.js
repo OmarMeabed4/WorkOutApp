68d39e79a224d3118d2f5818b8ccd79bea34beb1
Omar Meabed <omarmeabed4@gmail.com>
2022-08-15 14:44:14 -0700

    asdasd

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
 const express = require("express");
+const router = require("./routs/workouts");
 
 const app = express();
 
+app.use(express.json());
 app.use((req, res, next)=>{
     console.log(req.path, req.method);
     next();
 });
 
-app.get("/", (req, res,)=>{
-    res.json({
-        message:"yooo hooo droooogoz"
-    });    
-});
+app.use("/api/workouts", router);
 
 app.listen(process.env.PORT, ()=>{
     console.log("hellooooo i am  listennning to ",process.env.PORT);
