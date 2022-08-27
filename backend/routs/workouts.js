6ca2cbd69ab9d01d435ea9bae9b09c7c90aece3d
Omar Meabed <omarmeabed4@gmail.com>
2022-08-26 23:49:39 -0700

    sd

--- backend/routs/workouts.js
+++ backend/routs/workouts.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const Workout = require("../models/workoutModel")
 
 const router = express.Router()
 
@@ -14,10 +15,17 @@ router.get('/:id', (req, res)=> {
     })
 })
 
-router.post('/', (req, res)=> {
-    res.json({
-        message: "pooosstttting respomnse"
-    })
+router.post('/', async (req, res)=> {
+    const {title, reps, load} = req.body
+
+    try {
+        const workout = await Workout.create({title, reps, load})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+
 })
 
 router.delete('/:id', (req, res)=> {
