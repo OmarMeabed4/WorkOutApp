430de308eeee0da8e6be4092030f3a2ff9943244
Omar Meabed <omarmeabed4@gmail.com>
2022-08-26 22:13:27 -0700

    ss

--- backend/routs/workouts.js
+++ backend/routs/workouts.js
@@ -1,35 +1,35 @@
-const express = require("express");
+const express = require("express")
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res)=> {
     res.json({
         message: "getttting respomnse"
-    });
-});
+    })
+})
 
 router.get('/:id', (req, res)=> {
     res.json({
         message: "getttting respomnse from id"
-    });
-});
+    })
+})
 
 router.post('/', (req, res)=> {
     res.json({
         message: "pooosstttting respomnse"
-    });
-});
+    })
+})
 
 router.delete('/:id', (req, res)=> {
     res.json({
         message: "deleeetttting respomnse with id"
-    });
-});
+    })
+})
 
 router.patch('/:id', (req, res)=> {
     res.json({
         message: "updaaatttting respomnse"
-    });
-});
+    })
+})
 
-module.exports = router;
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,24 @@
-require('dotenv').config();
-const express = require("express");
-const router = require("./routs/workouts");
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require("express")
+const router = require("./routs/workouts")
+const mongoose = require("mongoose")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 app.use((req, res, next)=>{
-    console.log(req.path, req.method);
-    next();
-});
+    console.log(req.path, req.method)
+    next()
+})
 
-app.use("/api/workouts", router);
+app.use("/api/workouts", router)
 
 mongoose.connect(process.env.mongodb_URI)
     .then(()=>{
         app.listen(process.env.PORT, ()=>{
-            console.log("hellooooo i am conneccted to the adtabase and  listennning to " + process.env.PORT);
-        });
+            console.log("hellooooo i am conneccted to the adtabase and  listennning to " + process.env.PORT)
+        })
     })
     .catch((err)=>{
-        console.log(err);
-    });
-
+        console.log(err)
+    })
